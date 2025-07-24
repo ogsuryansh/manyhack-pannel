31d5af69e269ed509db5c5681cf92a68f2e44934
ogsuryansh <suryansh1885@gmail.com>
2025-07-24 14:03:47 +0530

    updated mongo

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -9,7 +9,12 @@ if (!cached) {
 async function connectToDatabase() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      tlsAllowInvalidCertificates: false, // set to true ONLY if using self-signed certs locally
+    }).then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
